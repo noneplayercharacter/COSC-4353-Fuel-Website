6df1d4d4042c47576238d07875bfd8d7cf2e6f53
Anthony Castillo <508ajc@gmail.com>
2023-10-13 15:07:55 -0500

    modify account backend

--- REACT/server/server.js
+++ REACT/server/server.js
@@ -1,34 +1,73 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
+const bodyParser = require('body-parser');
+
 
 /*app.get("/api", (req, res) => {
     res.json({ message: "Hello from server!" });
 });
 */
 // GET Directories
+let userData = {
+    fullName: 'Ricky Bobby',
+    address1: '123 ShakenBake Dr',
+    address2: 'Apt 456',
+    city: 'Houston',
+    state: 'TX',
+    zipcode: '98745',
+  };
+  function updateUser(data) {
+    if (!data.fullname || data.fullname.length === 0 || data.fullname.length > 50) {
+      return { isValid: false, message: 'Invalid full name' };
+    }
+    userData = { ...userData, ...data };
+  
+    return { isValid: true, data: userData };
+  }
+app.use(bodyParser.json());
+
 app.get("/api", (req, res) => {
     res.json({ message: "Hello from server!" });
 });
 
 app.get("/api/Quote", (req, res) => {
-    res.json({ gallons: 0,
-               address: "8251 bob street",
-               date: "2022-01-01",
-               price: 2.5,
-               total: 0});
+    res.json({
+        gallons: 0,
+        address: "8251 bob street",
+        date: "2022-01-01",
+        price: 2.5,
+        total: 0
+    });
 });
 
 app.get("/api/QuoteHistory", (req, res) => {
-    res.json({ gallons: 20,
-               address: "1512 john street",
-               date: "2022-01-01",
-               price: 2.50,
-               total: 50});
+    res.json({
+        gallons: 20,
+        address: "1512 john street",
+        date: "2022-01-01",
+        price: 2.50,
+        total: 50
+    });
+});
+app.get("/api/modifyAccount", (req, res) => {
+    res.json(userData);
+});
+app.post('/api/modifyAccount', (req, res) => {
+    userData.fullName = req.body.fullName;
+    userData.firstAddress = req.body.firstAddress;
+    userData.secondAddress = req.body.secondAddress;
+    userData.city = req.body.city;
+    userData.state = req.body.state;
+    userData.zipcode = req.body.zipcode;
+
+    res.json({ message: 'User account updated successfully' });
 });
 
 
+
+
 // Port in-use msg
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
     console.log(`Server running on port:${PORT}`)
 });
--- REACT/src/pages/ModifyAccount/ModifyAccount.js
+++ REACT/src/pages/ModifyAccount/ModifyAccount.js
@@ -1,15 +1,49 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from './ModifyAccount.module.css'
 
 //Function that get's the address of where the data is.
 //Also keeps code organized
 
 function ModifyAccount(){
+    const [data, setData] = useState({
+        fullName: "",
+        address1: "",
+        address2: "",
+        city: "",
+        state: "",
+        zipcode: "",
+      });
+    
+      useEffect(() => {
+        // Fetch user data from the backend
+        fetch('/api/modifyAccount')
+          .then((res) => res.json())
+          .then((data) => setData(data));
+      }, []);
+    
+      const handleFormSubmit = (e) => {
+        e.preventDefault();
+    
+        // Send user data to the backend to update the account
+        fetch('/api/modifyAccount', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(data),
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            // Handle the response from the server (success or error)
+            alert(data.message);
+          });
+      };
+    
     return(
     <div>
         <div className="container">
             <h1>Modify Account</h1>
-            <form>
+            <form onSubmit={handleFormSubmit}>
             <label htmlFor="full-name">Full Name:</label>
             <input
                 type="text"
@@ -17,6 +51,8 @@ function ModifyAccount(){
                 name="full-name"
                 maxLength={50}
                 required=""
+                value={data.fullName}
+                onChange={(e) => setData({ ...data, fullName: e.target.value })}
             />
             <label htmlFor="address1">Address 1:</label>
             <input
@@ -25,17 +61,20 @@ function ModifyAccount(){
                 name="address1"
                 maxLength={100}
                 required=""
+                value={data.address1}
+                onChange={(e) => setData({ ...data, address1: e.target.value })}
             />
             <label htmlFor="address2">Address 2:</label>
-            <input type="text" id="address2" name="address2" maxLength={100} />
+            <input type="text" id="address2" name="address2" maxLength={100}  value={data.address2} onChange={(e) => setData({ ...data, address2: e.target.value })}/>
             <label htmlFor="city">City:</label>
-            <input type="text" id="city" name="city" maxLength={100} required="" />
+            <input type="text" id="city" name="city" maxLength={100} required=""  value={data.city} onChange={(e) => setData({ ...data, city: e.target.value })}/>
             <label htmlFor="state">State:</label>
             <select className = "container-input container-select" id="state" name="state" required="">
                 <option value="" disabled="" selected="">
                 Select State
                 </option>
                 <option value="AL">gotta add from DB</option>
+                <option value={data.state}>TX</option>
             </select>
             <label htmlFor="zipcode">Zipcode:</label>
             <input
@@ -45,6 +84,8 @@ function ModifyAccount(){
                 pattern=".{5,}"
                 maxLength={9}
                 required=""
+                value={data.zipcode}
+                onChange={(e) => setData({ ...data, zipcode: e.target.value })}
             />
             <button type="submit">Create Account</button>
             </form>
