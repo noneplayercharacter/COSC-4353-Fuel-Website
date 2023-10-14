bc8c686689d54e6df606477b50d1a4b28dd87a53
Anthony Castillo <508ajc@gmail.com>
2023-10-13 20:57:16 -0500

    updated client profile module

--- REACT/server/server.js
+++ REACT/server/server.js
@@ -9,12 +9,12 @@ app.use(express.json());
 app.use(cors());
 let userData = {
     fullName: 'Ricky Bobby',
-    address1: '123 ShakenBake Dr',
+    address1: '1234 ShakenBake Dr',
     address2: 'Apt 456',
     city: 'Houston',
     state: 'TX',
     zipcode: '98745',
-  };
+};
 
 
 
@@ -26,11 +26,12 @@ app.get("/api", (req, res) => {
 // Get for Quote.js, Gets data from server(Can be changed to DB)
 app.get("/api/Quote", (req, res) => {
     res.json({
-            gallons: 0,
-            address: "8251 bob street",
-            date: "2022-01-01",
-            price: 2.5,
-            total: 0});
+        gallons: 0,
+        address: "8251 bob street",
+        date: "2022-01-01",
+        price: 2.5,
+        total: 0
+    });
 });
 
 
@@ -49,40 +50,82 @@ const quoteSchema = Yup.object({
 app.post('/api/validateQuote', (req, res) => {
     const formData = req.body;
     quoteSchema.validate(formData).catch(err => {
-        res.status(400).json({ errors: [err.errors]});
+        res.status(400).json({ errors: [err.errors] });
     }).then(valid => {
         if (valid) {
             formData.total = formData.gallons * formData.price;
             res.json({ formData })
         }
-    })});
+    })
+});
 
 
 // Get for QuoteHistory.js, Gets data from server(Can be changed to DB)
 app.get("/api/QuoteHistory", (req, res) => {
-    res.json({ gallons: 20,
-               address: "1512 john street",
-               date: "2022-01-01",
-               price: 2.50,
-               total: 50});
+    res.json({
+        gallons: 20,
+        address: "1512 john street",
+        date: "2022-01-01",
+        price: 2.50,
+        total: 50
+    });
+});
+
+
+const accountSchema = Yup.object({
+    fullName: Yup.string()
+        .required("Name is required")
+        .min(2, "Name must be greater than 1 characters")
+        .max(50, "Name cannot exceed 50 characters"),
+    address1: Yup.string()
+        .required("Address is required")
+        .min(2, "Address must be greater than 2")
+        .max(100, "Address must be less than 100"),
+    address2: Yup.string()
+        .optional("second Address is optional")
+        .min(2, "Address must be greater than 2")
+        .max(100, "Address must be less than 100"),
+    city: Yup.string()
+        .required("Address is required")
+        .min(2, "City must be greater than 2")
+        .max(100, "City must be less than 100"),
+    zipcode: Yup.string()
+        .required("Address is required")
+        .min(5, "Zip-Code must be at least 5")
+        .max(9, "Zip-Code must be at less than 9"),
 });
 
 app.get("/api/modifyAccount", (req, res) => {
     res.json(userData);
 });
 app.post('/api/modifyAccount', (req, res) => {
-    userData.fullName = req.body.fullName;
+    const formData = req.body;
+
+  accountSchema.validate(formData)
+    .then((valid) => {
+      if (valid) {
+        userData = { ...userData, ...valid };
+        res.json({ message: 'User account updated successfully', formData: valid });
+      } else {
+        res.status(400).json({ errors: ["Invalid Input"] });
+      }
+    })
+    .catch((err) => {
+      res.status(400).json({ errors: err.errors });
+    });
+
+    /*userData.fullName = req.body.fullName;
     userData.firstAddress = req.body.firstAddress;
     userData.secondAddress = req.body.secondAddress;
     userData.city = req.body.city;
     userData.state = req.body.state;
     userData.zipcode = req.body.zipcode;
 
-    res.json({ message: 'User account updated successfully' });
+    res.json({ message: 'User account updated successfully' });*/
 });
 
 
 // Port in-use msg
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
     console.log(`Server running on port:${PORT}`)
 });
